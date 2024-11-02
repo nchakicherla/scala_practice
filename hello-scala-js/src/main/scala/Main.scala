e10a0a9804e05d39e4305925117431968726df88
nchakicherla <nchaki6@icloud.com>
2024-11-01 23:51:35 -0700

    cleanup

--- hello-scala-js/src/main/scala/Main.scala
+++
@@ -1,9 +0,0 @@
-// src/main/scala/Main.scala
-import scala.scalajs.js.annotation._
-
-@JSExportTopLevel("Main")
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello, Scala.js!")
-  }
-}
